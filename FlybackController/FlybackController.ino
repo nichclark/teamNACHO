3d0e57000563968d040b9db9a41dac0803501dce
ahasti <adam.hastings@maine.edu>
2015-10-22 21:53:45 -0400

    First attempt at complete PI Loop
    Used code to drive a DC fan. Used a pot to set the desired speed.

--- FlybackController/FlybackController.ino
+++ FlybackController/FlybackController.ino
@@ -1,39 +1,52 @@
 // PID Loop Program
 
 
-//int ledPin = 7;
+int PWMOutput = 6;
 int pot = A0;
-double kp = 1;
-double ki = 10;
-double PTerm, ITerm;
-double Error;
-double Desired, Actual;
-double DutyCycle;
+int kp = 1;
+int ki = 2;
+int PTerm, ITerm, SumTerm;
+int Error, NewError;
+int PrevError = 0;
+int Desired, Actual;
+int DutyCycle;
+long SampleTime = 200; //miliseconds
+long CurrentTime, LastTime;
+int feedback = A1;
 
 void setup() {
-//  TCCR0B = TCCR0B & B11111000 | B00000001;     // Prescale factor of 1 to get a Fpwm = 62.5kHz
-
+  TCCR0B = TCCR0B & B11111000 | B00000001;     // Prescale factor of 1 to get a Fpwm = 62.5kHz
+  Serial.begin(9600);
+  millis();
+  //pinMode(pot,INPUT);
+  //pinMode(PWMOutput,OUTPUT);
 }
 
 void loop() {
-//   analogWrite(ledPin, duty); 
-
-  Desired = analogRead(pot);    //pot value between 0 and 1024
-  Actual = analogRead(feedback); //feedback from output voltage
-  Error = Desired - Actual;   //calculate the error
-
-  PTerm = Error * kp;   //multiply error by proportional term
   
+  CurrentTime = millis()/64;
   
+  if(CurrentTime >= LastTime + SampleTime){
+    Desired = analogRead(pot);    //pot value between 0 and 1024
+    Actual = analogRead(feedback); //feedback from output voltage
+    Error = Desired - Actual;   //calculate the error
   
+    PTerm = Error * kp;   //multiply error by proportional term
+    NewError = ki * Error;
+    ITerm = NewError - PrevError;
+    SumTerm = PTerm + ITerm;
+    
+    PrevError = NewError;
 
+    LastTime = CurrentTime;
+    
+    DutyCycle = map(SumTerm, 0, 1024, 0, 255);
+    Serial.println(DutyCycle);
+   // Serial.println(Error);
+   // Serial.println(NewError);
 
-
-
-
-
-
-
-
+  }
+  
+  analogWrite(PWMOutput, DutyCycle);
 
 }
