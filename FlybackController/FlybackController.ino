7f76741dd27ced69d35a5d7e38ce45fb32e3a939
nichclark <clark.nicholasp@gmail.com>
2015-10-22 21:15:53 -0400

    Testing the analogWrite function

--- FlybackController/FlybackController.ino
+++ FlybackController/FlybackController.ino
@@ -1,9 +1,13 @@
+int ledPin = 6;
+int duty = 127;
 void setup() {
   // put your setup code here, to run once:
+  TCCR0B = TCCR0B & B11111000 | B00000001;     // Prescale factor of 1 to get a Fpwm = 62.5kHz
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+   analogWrite(ledPin, duty);
+     
 }
