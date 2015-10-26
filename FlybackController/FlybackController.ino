7f8c6c630477b5dd205649c6425d6e8e9c194416
ahasti <adam.hastings@maine.edu>
2015-10-25 20:46:10 -0400

    Redid PID loop program, have not tested yet

--- FlybackController/FlybackController.ino
+++ FlybackController/FlybackController.ino
@@ -1,52 +1,52 @@
 // PID Loop Program
 
-
+// ------------------ Pin Set-Up ----------------------//
 int PWMOutput = 6;
 int pot = A0;
-int kp = 1;
-int ki = 2;
-int PTerm, ITerm, SumTerm;
-int Error, NewError;
-int PrevError = 0;
-int Desired, Actual;
-int DutyCycle;
-long SampleTime = 200; //miliseconds
-long CurrentTime, LastTime;
 int feedback = A1;
+// ------------------ Declare Constant Variables ---------------------//
+int P, I, D, Duty;
+int Error, Last;
+int Desire, Actual;
+int MaxIntegral, Integral;
+long To, Tf;
+int Scale = 0.25;
+//------------------- Declare Adjustable Variables -------------------//
+int kP = 1;
+int kI = 0;
+int kD = 0;
+long dT = 200; //miliseconds
 
 void setup() {
   TCCR0B = TCCR0B & B11111000 | B00000001;     // Prescale factor of 1 to get a Fpwm = 62.5kHz
   Serial.begin(9600);
   millis();
-  //pinMode(pot,INPUT);
-  //pinMode(PWMOutput,OUTPUT);
+
 }
 
 void loop() {
   
-  CurrentTime = millis()/64;
-  
-  if(CurrentTime >= LastTime + SampleTime){
-    Desired = analogRead(pot);    //pot value between 0 and 1024
-    Actual = analogRead(feedback); //feedback from output voltage
-    Error = Desired - Actual;   //calculate the error
-  
-    PTerm = Error * kp;   //multiply error by proportional term
-    NewError = ki * Error;
-    ITerm = NewError - PrevError;
-    SumTerm = PTerm + ITerm;
-    
-    PrevError = NewError;
+   Desire = analogRead(pot);
+   Actual = analogRead(feedback);
+   Error = Desire - Actual;
 
-    LastTime = CurrentTime;
-    
-    DutyCycle = map(SumTerm, 0, 1024, 0, 255);
-    Serial.println(DutyCycle);
-   // Serial.println(Error);
-   // Serial.println(NewError);
-
+  if (abs(Error) < MaxIntegral){ // prevent integral 'windup'
+    Integral = Integral + Error; // accumulate the error integral
   }
+  else {
+    Integral=0; // zero it if out of bounds
+  }
+  P = Error * kP; // calc proportional term
+  I = Integral * kI; // integral term
+  D = (Last - Actual) * kD; // derivative term
+  Duty = P + I + D; // Total  = P+I+D
+  Duty = Duty * Scale; // scale  to be in the range 0-255
   
-  analogWrite(PWMOutput, DutyCycle);
+  if (abs(Duty)>255) {
+    Duty = 255;
+  }
+  Last = Actual; // save current value for next time
+   
+   analogWrite (PWMOutput,Duty); // send PWM command
 
 }
