7f80203a74c5826c8e87ceb4eb790929449efa3c
nichclark <clark.nicholasp@gmail.com>
2015-10-27 10:40:17 -0400

    Added kD and subroutine
    Move PID loop into subroutine to make main loop simpler. Also added
    derivative term calculations to program. Hopefully this will help reduce
    SSE.

--- FlybackController/FlybackController.ino
+++ FlybackController/FlybackController.ino
@@ -3,55 +3,68 @@
 
 int PWMOutput = 6;
 int pot = A0;
-int kP = 1.75;
-int kI = 0.9;
-int PTerm, ITerm, SumTerm;
-int Error, NewError;
-int PrevError = 0;
-int Desired, Actual;
-int DutyCycle;
-long SampleTime = 10; //miliseconds
-long CurrentTime, LastTime;
 int feedback = A1;
+/*-------------------------------------------*/
+int kP = 1.5;
+int kD = 0;
+int kI = 0;
+int IntegralRange = 25;
+long SampleTime = 100; //miliseconds
+/*-------------------------------------------*/
+//int PTerm, ITerm, SumTerm;
+//int Error, NewError;
+//int PrevError = 0;
+//int Desired, Actual;
+
+
 
 void setup() {
   TCCR0B = TCCR0B & B11111000 | B00000001;     // Prescale factor of 1 to get a Fpwm = 62.5kHz
   Serial.begin(9600);
   millis();
-  //pinMode(pot,INPUT);
-  //pinMode(PWMOutput,OUTPUT);
 }
 
 void loop() {
+  int DutyCycle;
+  long LastTime;
   
-  CurrentTime = millis()/64;
+  long CurrentTime = millis()/64;
   
   if(CurrentTime >= LastTime + SampleTime){
-    Desired = analogRead(pot);    //pot value between 0 and 1024
-    Actual = analogRead(feedback); //feedback from output voltage
-    Error = Desired - Actual;   //calculate the error
-  
-    PTerm = kP * Error;   //multiply error by proportional term
-    NewError = kI * Error;
-    ITerm = NewError - PrevError;
-    SumTerm = PTerm + ITerm;
-    
-    PrevError = NewError;
-
+    DutyCycle = PIDcontroller();
     LastTime = CurrentTime;
-    
-    DutyCycle = map(SumTerm, 0, 1024, 0, 255);
-    if(DutyCycle > 255){
-      DutyCycle = 200;
-    }
-    if(DutyCycle < 0){
-      DutyCycle = 5;
-    }
-    Serial.println(DutyCycle);
-   // Serial.println(Error);
-   // Serial.println(NewError);
   }
-  
   analogWrite(PWMOutput, DutyCycle);
 
 }
+int PIDcontroller(){
+  int Last;
+  int Integral;
+  int Desired = analogRead(pot);    //pot value between 0 and 1024
+  int Actual = analogRead(feedback); //feedback from output voltage
+  int Error = Desired - Actual;   //calculate the error
+  
+  if(abs(Error) < IntegralRange){
+    Integral = Integral + Error;
+  }
+  else{
+    Integral = 0;
+  }
+  
+  int P = Error * kP;   //multiply error by proportional term
+  int I = Integral * kI;
+  int D = (Last - Actual) * kD;
+  int Sum = P + I + D;
+ 
+  int Duty = map(Sum, 0, 1024, 0, 255);
+  if(Duty > 255){
+    Duty = 255;
+  }
+  if(Duty < 0){
+    Duty = 0;
+  }
+  Last = Actual;
+  Serial.println(Duty);
+  return Duty;
+}
+
