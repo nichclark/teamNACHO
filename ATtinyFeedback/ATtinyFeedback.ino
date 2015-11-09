ce0f180749f0410e057242db6c7881831f8a24ae
nichclark <clark.nicholasp@gmail.com>
2015-11-08 21:30:13 -0500

    ATtiny Program
    Decided to use an ATtiny85 to take the output voltage and create a
    signal. The signal will pass through an opto. This will allow us to have
    isolation between the two sides of the circuit. Right now it takes the
    voltage to control a duty cycle, may have it control frequency instead?
    Not sure yet.

---
+++ ATtinyFeedback/ATtinyFeedback.ino
@@ -0,0 +1,19 @@
+/*
+ATtiny85 PinOut
+      Reset |1   o   8| VCC+
+AI3/DO-Pin3 |2       7| AI1-Pin2
+AI2/DO-Pin4 |3       6| PWM-Pin1
+        GND |4       5| PWM-Pin0
+
+*/
+int Out = 0;
+int FBack = 1;
+void setup() {
+  pinMode(Out, OUTPUT);
+  //pinMode(FBack, INPUT);
+}
+void loop() {
+  int value = analogRead(FBack);
+  int duty = value/4;
+  analogWrite(Out,duty);
+}
