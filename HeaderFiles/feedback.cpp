851aa690c5fd0c7d9d6e0537a74ce2b8623cf599
nichclark <clark.nicholasp@gmail.com>
2015-12-07 14:18:35 -0500

    Feedback
    This header file reads a digital pin and determines what the duty cycle
    is. This is how the feedback of the output voltage is determined

---
+++ HeaderFiles/feedback.cpp
@@ -0,0 +1,28 @@
+﻿#include "feedback.h"
+#include <arduino.h>
+#include <avr/io.h>
+
+int calcFeedback(int feedback){
+
+	//Reads a PWM signal's duty cycle and frequency.
+	double duty;
+	double Vfb;
+	long highTime = 0;
+	long lowTime = 0;
+	long tempPulse;
+	
+	tempPulse = pulseIn(feedback,HIGH);
+	if(tempPulse>highTime){
+		highTime = tempPulse;
+	}
+	tempPulse = pulseIn(feedback,LOW);
+	if(tempPulse>lowTime){
+		lowTime = tempPulse;
+	}
+	duty = highTime/(double (lowTime+highTime));
+
+	Vfb = 255*duty;
+	Vfb = map(Vfb, 0, 255, 0, 1024);
+	
+	return (int) Vfb;
+}
---
+++ HeaderFiles/feedback.h
@@ -0,0 +1,8 @@
+#ifndef FEEDBACK_H
+#define FEEDBACK_h
+
+
+int calcFeedback (int feedback);
+
+
+#endif DISPLAY_H
