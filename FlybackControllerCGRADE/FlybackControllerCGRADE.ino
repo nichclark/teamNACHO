8caa8afc1f711ef729ac8915176f72e00453344a
nichclark <clark.nicholasp@gmail.com>
2015-12-07 11:36:44 -0500

    Comments complete for CGrade Code
    Comments Done.

--- FlybackControllerCGRADE/FlybackControllerCGRADE.ino
+++ FlybackControllerCGRADE/FlybackControllerCGRADE.ino
@@ -1,44 +1,24 @@
-// PID Loop Program
-
-/*
-For 1V:
-kP = 1.6
-kI = 1
-kD = 1
-
-50ohm = 74/255 duty
-68ohm = 71/255 duty
-100ohm = 67/255 duty
-200ohm = 60/255 duty
-5kohm = couldn't get with in 50mV tolerance
-
-For 5V:
-kP = 1.4
-kI = 1
-kD = 1
-
-50ohm = 144/255 duty
-68ohm = 138/255 duty
-100ohm = 131/255 duty
-200ohm = 128/255 duty
-5kohm = couldn't get with in 250mV tolerance (105/255)=5.45V
-
-For 12V:
-kP = 1.4
-kI = 1
-kD = 1
-
-50ohm = /255 duty
-68ohm = /255 duty
-100ohm = /255 duty
-200ohm = -/255 duty
-5kohm = couldn't get with in 250mV tolerance (105/255)=5.45V
-
-*/
-
+/* TeamNACHO's "NACHO Power" USB Power Supply- CGrade Code
+ *  By: Nicholas Clark & Adam Hastings
+ *  Date Complete: 07 December 2015
+ *  
+ *  The following code is for the CGrade Power supply. For the CGrade,
+ *  the two sides of the circuit (primary and secondary) are tied
+ *  together. As a result, the feedback from the output voltage can
+ *  be read through an analog pin. The user sets the output voltage
+ *  through the use of a potentiometer, which is also read through
+ *  an analog pin. These two values are then used to calculate the
+ *  error. The error is then used in a PID loop. Through trial and
+ *  error, it was found that a PI loop was all that was needed. For
+ *  the CGrade, there was no subfunctions or header files used/created.
+ *  This will be done for above and beyond circuit and for the PCB.
+ *  
+ */
+// These variables correspond to pins on the Arduino
 int PWMOutput = 6;
 int pot = A0;
 int feedback = A1;
+// These global variables are initialized and set to zero
 long LastTime = 0;
 int OutputSignal = 0;
 int Last = 0;
@@ -52,93 +32,79 @@ int Duty = 0;
 int Actual = 0;
 int Desired = 0;
 
-/*-------------------------------------------*/
-int kP = 1.05;
-int kI = 0;
-int kD = 1;
-int IntegralRange = 500;
-long SampleTime = 50; //miliseconds
-/*-------------------------------------------*/
+//The follow varables are what get used in the PID loop
+int kP = 0.2; //proportional constant
+int kI = 1; //integral constant
+int kD = 0; //derivative constant
+int IntegralRange = 500; //sets when the intergral term start taking effect
+long SampleTime = 50; //how often PID loop runs in miliseconds
 
 void setup() {
+  //TCCR0B is the internal clock the program uses. The default scale 
+  //factor for this clock is 0x04, making the clock frequency 976.5625 Hz.
+  //By changing the scale factor to 0x01, the new clock frequency is
+  //62500 Hz. This change in clock speed had to be done so that the output
+  //pin, pin6, would be at 62.5kHz. This information is from
+  //http://playground.arduino.cc/Main/TimerPWMCheatsheet
   TCCR0B = (TCCR0B & 0b11111000) | 0x01;
-  Serial.begin(9600);
-  
+  Serial.begin(9600); //Baud rate of serial monitor used for debugging
 }
   
 void loop() {
-  int OutputSignal;
+  //Since clock was changed to 62.5kHz, had to divide clock by 64(0x04) to
+  //be able to look at the clock in milliseconds
   long CurrentTime = millis()/64;
+  //Reading the user input
   Desired = analogRead(pot);
-  //Desired = 200;
-  Actual = analogRead(feedback);  
-  
-  if(CurrentTime - LastTime > SampleTime){
-    /*OutputSignal = PIDcontroller(Desired,Actual);
-    */
+  //Reading the output voltage 
+  Actual = analogRead(feedback); 
 
-    Error = Desired - Actual;   //calculate the error
+  //How oftem the PID loop runs is set by the SampleTime Variable.
+  if(CurrentTime - LastTime > SampleTime){
+    //Calculate the error in the output voltage
+    Error = Desired - Actual;   
+    //Determine if the Integral should be used in the PID calculation.
+    //The integral is found by adding the new error to the prevous error
+    //By having a range on the Integral, this prevents the integral
+    //value from getting too large.
+    if(abs(Error) < IntegralRange) Integral = Integral + Error;
+    else Integral = 0;
+    //The terms of the PID loop are calculated below:
+    //Multiply error by proportional constant
+    P = Error * kP;
+    //Multiply the integral term by the integral constant
+    I = Integral * kI;
+    //Determine the differenece between the current user input value and
+    //the last user input. This is then multiplied by the derivative constant
+    D = (Last - Actual) * kD;
+    //Add the three terms together to determine the output duty cycle
+    Duty = P + I + D;
+    //A max and min limit were set on the output duty cycle. This was done
+    //to protect the harware.
+    if(Duty > 1000) Duty = 1000;
+    if(Duty < 0) Duty = 0; 
+    //The map fxn takes a number between 0 and 1024 and converts it
+    //to be between 0 and 255. This is needed as the analogWrite fxn
+    //needs a value between 0 and 255.
+    DutyCycle = map(Duty, 0, 1024, 0, 255); 
+    //Save the current user input to be used to calculate the derivative
+    Last = Actual;
+    //Save the current time to make sure the PID loop runs at the correct
+    //sample time
+    LastTime = CurrentTime;
+    //The following serial print commands were used for debug. Wanted to see
+    //how the PID loop was responding to the error and what the output
+    //duty cycle was being set to as a 10bit and 8bit number.
     Serial.print("Error equals ");
     Serial.print(Error);Serial.print(", ");
-    if(abs(Error) < IntegralRange){
-      Integral = Integral + Error;
-    }
-    else{
-      Integral = 0;
-    }
-    P = Error * 0.2;//multiply error by proportional term
-    I = Integral * 1;
-    Duty = P + I;
-  
-    if(Duty > 1000){
-      Duty = 1000;
-    }
-    if(Duty < 0){
-      Duty = 0;
-    } 
-    // map fxn takes a number between 0 and 1024 and converts it
-    // to be between 0 and 255
-    DutyCycle = map(Duty, 0, 1024, 0, 255); 
-    
-    Last = Actual;
-    LastTime = CurrentTime;
     Serial.print("Output Duty = ");
     Serial.print(Duty);Serial.print(", ");
     Serial.println(DutyCycle);
   }
+  //The duty cycle controls the output of this pin. Since this command is
+  //outside of the PID loop, the pin with have a constant duty cycle for 
+  //every clock cycle until the PID loop is run. When the PID loop is run,
+  //the output duty cycle is changed and will now be constant till the next time.
   analogWrite(PWMOutput, DutyCycle);
-  
 }
-/*
-int PIDcontroller(int Desired, int Actual){
-  int Last;
-  int Integral;
-  int Error = Desired - Actual;   //calculate the error
-  Serial.print("Error equals ");
-  Serial.println(Error);
-  if(abs(Error) < IntegralRange){
-    Integral = Integral + Error;
-  }
-  else{
-    Integral = 0;
-  }
-  
-  int P = Error * kP;   //multiply error by proportional term
-  int I = Integral * kI;
-  int D = (Last - Actual) * kD;
-  int Duty = P + I + D;
 
-  if(Duty > 225){
-    Duty = 225;
-  }
-  if(Duty < 0){
-    Duty = 0;
-  } 
-  int DutyCycle = map(Duty, 0, 1024, 0, 255);
-  
-  Last = Actual;
-  Serial.print("DutyCycle equals ");
-  Serial.print(Duty);Serial.print(",");Serial.println(DutyCycle);
-  return DutyCycle;
-}
-*/
