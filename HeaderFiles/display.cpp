88db80cb2b5fb91e914a26d67f1401454cfc9a82
nichclark <clark.nicholasp@gmail.com>
2015-12-07 20:33:25 -0500

    Don't need anymore
    Don't need header file anymore since matrix had to be declared in main
    code. However, was able to get decimal to print by calculating what
    digit needed to be displayed for each 7seg. This code is now all in the
    main code.

--- HeaderFiles/display.cpp
+++ HeaderFiles/display.cpp
@@ -4,12 +4,21 @@
 #include "Adafruit_LEDBackpack.h"
 #include "Adafruit_GFX.h"
 
-
 Adafruit_7segment matrix = Adafruit_7segment();
-void displayValue (int value){
-	matrix.print(value,DEC);
-	//matrix.writeDigitNum(1,true);
-	matrix.blinkRate(0);
-	matrix.drawColon(true);
+
+void displayValue (int value, int minValue, int maxValue){
+	matrix.begin(0x70);
+	int A;int B;int C;int D;
+	//value will have a max value of 1250
+	//value = map(value,0,1024,minValue,maxValue);
+	A = value/1000;
+	B = (value/100) - (A*10);
+	C = (value/10) - (A*100) - (B*10);
+	D = (value/1) - (A*1000) - (B*100) - (C*10);
+	
+	matrix.writeDigitNum(1,A);
+	matrix.writeDigitNum(2,B,true);
+	matrix.writeDigitNum(4,C);
+	matrix.writeDigitNum(5,D);
 	matrix.writeDisplay();
 }
--- HeaderFiles/display.h
+++ HeaderFiles/display.h
@@ -1,8 +1,7 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
-
-void displayValue (int value);
+void displayValue (int value, int minValue, int maxValue);
 
 
 #endif DISPLAY_H
