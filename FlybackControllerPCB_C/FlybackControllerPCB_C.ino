a61c942dc08236ce3191c723e453efd3b4aeaea4
nichclark <clark.nicholasp@gmail.com>
2015-12-12 16:19:08 -0500

    Meshing two PCBs together
    Trying to get the 7seg/PB pcb to work with CGrade circuit pcb. Using a
    desired value that is on 7seg is not working well in PID loop.

--- FlybackControllerPCB_C/FlybackControllerPCB_C.ino
+++ FlybackControllerPCB_C/FlybackControllerPCB_C.ino
@@ -11,7 +11,14 @@
  *  PB0-Pushbutton2 = D8
  *  
  */
- 
+#include <Wire.h>
+#include "Adafruit_LEDBackpack.h"
+#include "Adafruit_GFX.h"
+Adafruit_7segment matrix = Adafruit_7segment();
+
+#define maxValue 1250
+#define minValue 50
+
 // These variables correspond to pins on the Arduino
 int PWMOutput = 5;
 int pot = A0;
@@ -29,6 +36,24 @@ int D = 0;
 int Duty = 0;
 int Actual = 0;
 int Desired = 0;
+int button = 7;
+int button2 = 8;
+int bounceTime = 50;
+int holdTime = 2500;
+int x=0;
+
+int lastReading = HIGH;
+int lastReading2 = HIGH;
+
+int hold = 0;
+int hold2 = 0;
+
+int single = 0;
+int single2 = 0;
+
+long onTime = 0;
+long lastSwitchTime = 0;
+
 
 //The follow varables are what get used in the PID loop
 int kP = 0.2; //proportional constant
@@ -46,67 +71,109 @@ void setup() {
   //http://playground.arduino.cc/Main/TimerPWMCheatsheet
   TCCR0B = (TCCR0B & 0b11111000) | 0x01;
   Serial.begin(9600); //Baud rate of serial monitor used for debugging
+  matrix.begin(0x70);
   pinMode(5,OUTPUT);
   pinMode(7,INPUT);
   pinMode(8,INPUT);
   pinMode(A1,INPUT);
 }
-  
+
 void loop() {
-  //Since clock was changed to 62.5kHz, had to divide clock by 64(0x04) to
-  //be able to look at the clock in milliseconds
   long CurrentTime = millis()/64;
-  //Reading the user input
-  Desired = 125;
-  //Reading the output voltage 
+  Desired = userInput();
   Actual = analogRead(feedback); 
 
-  //How oftem the PID loop runs is set by the SampleTime Variable.
   if(CurrentTime - LastTime > SampleTime){
-    //Calculate the error in the output voltage
-    Error = Desired - Actual;   
-    //Determine if the Integral should be used in the PID calculation.
-    //The integral is found by adding the new error to the prevous error
-    //By having a range on the Integral, this prevents the integral
-    //value from getting too large.
+    Error = Desired - Actual;
     if(abs(Error) < IntegralRange) Integral = Integral + Error;
     else Integral = 0;
-    //The terms of the PID loop are calculated below:
-    //Multiply error by proportional constant
     P = Error * kP;
-    //Multiply the integral term by the integral constant
     I = Integral * kI;
-    //Determine the differenece between the current user input value and
-    //the last user input. This is then multiplied by the derivative constant
     D = (Last - Actual) * kD;
-    //Add the three terms together to determine the output duty cycle
     Duty = P + I + D;
-    //A max and min limit were set on the output duty cycle. This was done
-    //to protect the harware.
     if(Duty > 1000) Duty = 1000;
     if(Duty < 0) Duty = 0; 
-    //The map fxn takes a number between 0 and 1024 and converts it
-    //to be between 0 and 255. This is needed as the analogWrite fxn
-    //needs a value between 0 and 255.
     DutyCycle = map(Duty, 0, 1024, 0, 255); 
-    //Save the current user input to be used to calculate the derivative
     Last = Actual;
-    //Save the current time to make sure the PID loop runs at the correct
-    //sample time
     LastTime = CurrentTime;
-    //The following serial print commands were used for debug. Wanted to see
-    //how the PID loop was responding to the error and what the output
-    //duty cycle was being set to as a 10bit and 8bit number.
+
     Serial.print("Error equals ");
     Serial.print(Error);Serial.print(", ");
     Serial.print("Output Duty = ");
     Serial.print(Duty);Serial.print(", ");
     Serial.println(DutyCycle);
   }
-  //The duty cycle controls the output of this pin. Since this command is
-  //outside of the PID loop, the pin with have a constant duty cycle for 
-  //every clock cycle until the PID loop is run. When the PID loop is run,
-  //the output duty cycle is changed and will now be constant till the next time.
-  analogWrite(PWMOutput, DutyCycle);
+  analogWrite(PWMOutput, 125);
+  displayValue(Desired);
+  
 }
+void displayValue (int value){
+  int A;int B;int C;int D;
+  //value will have a max value of 1250
+  //value = map(value,0,1024,minValue,maxValue);
+  A = value/1000;
+  B = (value/100) - (A*10);
+  C = (value/10) - (A*100) - (B*10);
+  D = (value/1) - (A*1000) - (B*100) - (C*10);
+  
+  matrix.writeDigitNum(0,A);
+  matrix.writeDigitNum(1,B,true);
+  matrix.writeDigitNum(3,C);
+  matrix.writeDigitNum(4,D);
+  matrix.writeDisplay();
+}
+
+int userInput(){
+  int reading = digitalRead(button);
+  int reading2 = digitalRead(button2);
+
+  //first pressed
+  if (reading == LOW && lastReading == HIGH && reading2 == HIGH) {
+    onTime = millis();
+    x=x+10;
+  }
+  
+  //held
+  if (reading == LOW && lastReading == LOW && reading2 == HIGH) {
+    if ((millis() - onTime) > holdTime) {
+      x=x+10;
+      hold = 1;
+    }
+  }
+  
+  //released
+  if (reading == HIGH && lastReading == LOW) {
+    if (hold == 1) hold = 0;
+  }
+  
+  lastReading = reading;
+  
+  //DECREMENT SETUP
+  
+  
+  //first pressed
+  if (reading2 == LOW && lastReading2 == HIGH && reading == HIGH) {
+    onTime = millis();
+    x=x-10;
+  }
+  
+  //held
+  if (reading2 == LOW && lastReading2 == LOW && reading == HIGH) {
+    if ((millis() - onTime) > holdTime) {
+      x=x-10;
+      hold2 = 1;
+    }
+  }
+  
+  //released
+  if (reading2 == HIGH && lastReading2 == LOW) {
+    if (hold2 == 1) hold2 = 0;
+  }
+  if(x > maxValue) x = maxValue;
+  if(x < minValue) x = minValue;
+  lastReading2 = reading2;
+  //x = map(x,minValue,maxValue,0,1024);
+  return x;
+}
+
 
