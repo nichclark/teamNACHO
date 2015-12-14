bf83ee1c51bb40cbba14c5755a1b65df8e817903
nichclark <clark.nicholasp@gmail.com>
2015-12-14 12:08:35 -0500

    Feedback included with code
    This code now has code to read PWM input to calculate duty cycle. For
    some reason subroutines and header files are not working correctly. As
    of right now, are code is in main loop. Once program works, will try to
    recreate subroutines, get rid of global variables, etc.

--- FlybackControllerABOVE/FlybackControllerABOVE.ino
+++ FlybackControllerABOVE/FlybackControllerABOVE.ino
@@ -1,21 +1,9 @@
-/* TeamNACHO's "NACHO Power" USB Power Supply- Above and Beyond
- *  By: Nicholas Clark & Adam Hastings
- *  Date Complete: 07 December 2015
-
- *  This program will be used for above&beyond perf board circuit. 
- *  This code will include the implementation of the sevenseg display 
- *  and PBs as the user interface. It will also be reading a digital 
- *  pin as the feedback instead of an analog. This is done as the 
- *  feedback is being transmitted through an opto coupler so the circuit 
- *  will have isolation.
- *  
- */
 //Include the following header files
-#include "inits.h"
+//#include "inits.h"
 /*#include "display.h"*/
-#include "userinput.h"
-#include "PIDcontrol.h"
-#include "feedback.h"
+//#include "userinput.h"
+//#include "PIDcontrol.h"
+//#include "feedback.h"
 
 #include "Wire.h"
 #include "Adafruit_LEDBackpack.h"
@@ -23,43 +11,120 @@
 Adafruit_7segment matrix = Adafruit_7segment();
 
 //These variables correspond to pins on the Arduino
-#define Vopto 4
-#define PWMOutput OCR0A //pin D6
-#define ConstOutput OCR0B //pin D5
-//pin D7 = PB1
-//pin D8 = PB2
+int Vfeedback = 6; //pin D6
+int PBa = 7; //pin D7
+int PBb = 8; //pin D8
+int PWMOutput = 9; //pin D9
+int ConstOutput = 10; //pin D10
 //pin A4 = DAT for 7segDisplay
 //pin A5 = CLK for 7segDisplay
 
 //Define variables that set the max range of the power supply (x100)
-#define maxValue 1250
-#define minValue 50
-#define SetDuty 160
+int maxValue = 1250;
+int minValue = 50;
+int SetDuty = 160;
 // These global variables are initialized and set to zero
 int DutyCycle = 0;
+long CurrentTime = 0;
 long LastTime = 0;
 int Desired = 0;
 int Actual = 0;
 
-//The follow varables are what get used in the PID loop
-#define kP 0.1 //proportional constant
-#define kI 0 //integral constant
-#define kD 0 //derivative constant
-#define IntegralRange 500 //sets when the intergral term start taking effect
-#define SampleTime 500 //how often PID loop runs in miliseconds
+long pwm_value;
+long sum;
+long avg;
+double total;
+double value;
+long final[20];
+int i;
+int j;
+int maxx = 0;
 
-//Adafruit_7segment matrix = Adafruit_7segment();
+int bounceTime = 50;
+int holdTime = 25000;
+int x=0;
+int lastReading = HIGH;
+int lastReading2 = HIGH;
+int hold = 0;
+int hold2 = 0;
+int single = 0;
+int single2 = 0;
+long onTime = 0;
+long lastSwitchTime = 0;
+
+int Last = 0;
+int Integral = 0;
+int Error = 0;
+int P = 0;
+int I = 0;
+int D = 0;
+int Duty = 0;
+
+//The follow varables are what get used in the PID loop
+float kP = 0.2; //proportional constant
+float kI = 0.2; //integral constant
+float kD = 0; //derivative constant
+int IntegralRange = 500; //sets when the intergral term start taking effect
+int SampleTime = 25; //how often PID loop runs in miliseconds
+
 void setup() {
-  initializations(SampleTime);
+  TCCR1B = (TCCR1B & 0b11111000) | 0x01;
+  
+  //Baud rate of serial monitor used for debugging
+  Serial.begin(9600);
+  millis();
+  
+  pinMode(6,INPUT_PULLUP); // feedback pin
+  pinMode(7,INPUT); //PB1
+  pinMode(8,INPUT); //PB2
+  pinMode(9,OUTPUT); //Flyback gate PWM
+  pinMode(10,OUTPUT); //Constant gate PWM
   matrix.begin(0x70);
+  // Create arrray for the feedback calculation function to use
+  for (i=0; i<20; i++) final[i] = 0;
 }
 void loop() {
-  //Desired = userInput(minValue,maxValue);
-  Desired = map(5,0,1250,0,1024);
-  Actual = calcFeedback(Vopto); 
-  displayValue(Desired);
-  PWMOutput = DutyCycle;
-  ConstOutput = SetDuty;
+  CurrentTime = millis();
+  //Serial.println(CurrentTime);
+  Desired = userInput();
+  //Desired = map(5,0,1250,0,1024);
+  Desired = 600;
+  displayValue(150);
+  if(CurrentTime > LastTime + SampleTime){
+    //Actual = calcFeedback();
+    for(i=0; i<20; i++){ 
+     do{pwm_value = pulseIn(6, LOW);} while(pwm_value < 10);
+      final[i] = pwm_value;
+      if (i==19){
+        for (j=0; j<20; j++){
+          if(final[j] > maxx){maxx = final[j];}}}}
+    total = maxx/40;
+    Actual = 1024*(100-total)/100;
+
+    Error = Desired - Actual;
+    if(abs(Error) < IntegralRange) Integral = Integral + Error;
+    else Integral = 0;
+    P = Error * kP;
+    I = Integral * kI;
+    D = (Last - Actual) * kD;
+    Duty = P + I + D;
+    if(Duty > 1000) Duty = 1000;
+    if(Duty < 0) Duty = 0;
+    DutyCycle = map(Duty, 0, 1024, 0, 255);
+    Last = Actual;
+    Serial.print("Desired = ");Serial.print(Desired);Serial.print(", ");
+    Serial.print("Actual = ");Serial.print(Actual);Serial.print(", ");
+    Serial.print("Error = ");Serial.print(Error);Serial.print(", ");
+    Serial.print("Output Duty = ");Serial.print(Duty);Serial.print(", ");Serial.println(DutyCycle);
+
+  
+    
+    LastTime = CurrentTime;
+    maxx=0;
+  }
+  analogWrite(PWMOutput, DutyCycle);
+  analogWrite(ConstOutput,SetDuty);
+  
 }
 void displayValue (int value){
   int A;int B;int C;int D;
@@ -76,7 +141,106 @@ void displayValue (int value){
   matrix.writeDigitNum(4,D);
   matrix.writeDisplay();
 }
-ISR(TIMER1_COMPA_vect){
-  DutyCycle = PIDcontrol(kP,kI,kD,IntegralRange,Desired,Actual);
+
+
+int calcFeedback(){
+  
+  for(i=0; i<20; i++){
+    do
+    {
+      pwm_value = pulseIn(6, LOW);
+    } while(pwm_value < 10);
+
+    final[i] = pwm_value;
+
+    if (i==19){
+      for (j=0; j<20; j++){
+        if(final[j] > maxx){
+          maxx = final[j];
+        }
+     }
+    }  
+  }
+  //Serial.println(maxx);
+  total = maxx/40;
+  
+  value = 1024*(100-total)/100;
+  maxx=0;
+  
+  return (int) value;
 }
+
+
+int userInput(){
+
+  int reading = digitalRead(PBa);
+  int reading2 = digitalRead(PBb);
+
+  //first pressed
+  if (reading == LOW && lastReading == HIGH && reading2 == HIGH) {
+    onTime = millis();
+    x=x+5;
+  }
+  
+  //held
+  if (reading == LOW && lastReading == LOW && reading2 == HIGH) {
+    if ((millis() - onTime) > holdTime) {
+      x=x+5;
+      hold = 1;
+    }
+  }
+  
+  //released
+  if (reading == HIGH && lastReading == LOW) {
+    if (hold == 1) hold = 0;
+  }
+  
+  lastReading = reading;
+  
+  //DECREMENT SETUP
+  
+  
+  //first pressed
+  if (reading2 == LOW && lastReading2 == HIGH && reading == HIGH) {
+    onTime = millis();
+    x=x-5;
+  }
+  
+  //held
+  if (reading2 == LOW && lastReading2 == LOW && reading == HIGH) {
+    if ((millis() - onTime) > holdTime) {
+      x=x-5;
+      hold2 = 1;
+    }
+  }
+  
+  //released
+  if (reading2 == HIGH && lastReading2 == LOW) {
+    if (hold2 == 1) hold2 = 0;
+  }
+  if(x > maxValue) x = maxValue;
+  if(x < minValue) x = minValue;
+  lastReading2 = reading2;
+  return x;
+}
+
+
+void PIDcontrol(){
+  Error = Desired - Actual;
+  if(abs(Error) < IntegralRange) Integral = Integral + Error;
+  else Integral = 0;
+  P = Error * kP;
+  I = Integral * kI;
+  D = (Last - Actual) * kD;
+  Duty = P + I + D;
+  if(Duty > 1000) Duty = 1000;
+  if(Duty < 0) Duty = 0;
+  DutyCycle = map(Duty, 0, 1024, 0, 255);
+  Last = Actual;
+  Serial.print("Desired = ");Serial.print(Desired);Serial.print(", ");
+  Serial.print("Actual = ");Serial.print(Actual);Serial.print(", ");
+  Serial.print("Error = ");Serial.print(Error);Serial.print(", ");
+  Serial.print("Output Duty = ");Serial.print(Duty);Serial.print(", ");Serial.println(DutyCycle);
+}
+
 
